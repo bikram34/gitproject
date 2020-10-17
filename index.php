0855553477b7643f01a84bba581651a126030102
bikram34 <crbikulamichhane1234@gmail.com>
2020-10-17 20:49:37 +0545

    hello

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" type="text/css" href="style.css">
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+
+</body>
+</html>
